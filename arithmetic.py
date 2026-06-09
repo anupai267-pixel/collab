25a9266c1b37316a5ae3c7b93d9db714b5b8c71b
nidheesha07 <nidheesha2007@gmail.com>
2026-06-09 14:51:50 +0530

    update

--- arithmetic.py
+++ arithmetic.py
@@ -2,6 +2,13 @@ a=int(input("Enter first number:"))
 b=int(input("Enter second number:"))
 add=a+b
 diff=a-b
+mul=a*b
+if (b==0):
+    print("cannot devide by zero")
+else:
+    div=a/b
 print("Sum is:",add)
 print("Difference is:", diff)
+print("Multiplication is: ",mul)
+print("Division is : ",div)
 #arithmetic operation
